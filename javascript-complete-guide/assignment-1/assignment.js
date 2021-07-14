cc921d2c71e7ace6ecaa25e9699961319f323365
Quintin <quintin.balsdon@zuhlke.com>
2021-07-14 10:09:47 +0100

    assignment 1

---
+++ javascript-complete-guide/assignment-1/assignment.js
@@ -0,0 +1,11 @@
+const number = 0;
+let result;alert(result);
+
+result = 18 + number;
+
+result = result - 2;
+result = result * 5;
+result = result + 7;
+
+alert(`ORIGINAL VALUE: ${number}`);
+alert(`CALCULATED VALUE: ${result}`);
---
+++ javascript-complete-guide/basics-01/assets/scripts/app.js
@@ -0,0 +1,6 @@
+let currentResult = 14;
+
+let calculationDescription = `(${currentResult} + 10) * 3 / 2 - 1`
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+outputResult(currentResult, calculationDescription);
